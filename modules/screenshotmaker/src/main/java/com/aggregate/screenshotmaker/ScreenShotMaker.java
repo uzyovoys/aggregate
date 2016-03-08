48cba5ff28e5a55006d0e1f080097a83b2d11c82
popov <popovanton0@gmail.com>
2016-03-08 14:49:24 +0300

    directories without slashes

--- modules/screenshotmaker/src/main/java/com/aggregate/screenshotmaker/ScreenShotMaker.java
+++ modules/screenshotmaker/src/main/java/com/aggregate/screenshotmaker/ScreenShotMaker.java
@@ -9,13 +9,15 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by AntonPopov on 05.03.16.
  */
 public class ScreenShotMaker extends AbstractVerticle {
-    private static Logger log = LoggerFactory.getLogger(ScreenShotMaker.class);
     public static final String TEMP_PATH = System.getProperty("java.io.tmpdir");
+    private static Logger log = LoggerFactory.getLogger(ScreenShotMaker.class);
 
     @Override
     public void start() throws Exception {
@@ -24,7 +26,7 @@ public class ScreenShotMaker extends AbstractVerticle {
     }
 
     private void captureScreen() {
-        String fileName = "Screenshot_" + Math.random() + ".jpg";
+        String fileName = "/Screenshot_" + new SimpleDateFormat("yyyy-mm-dd:hh-mm").format(new Date()) + ".jpg";
         Robot robot;
         try {
             robot = new Robot();
@@ -34,10 +36,17 @@ public class ScreenShotMaker extends AbstractVerticle {
         }
 
         BufferedImage screenShot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
-        createDir(TEMP_PATH + "ScreenShot/");
-        File screenShotPathToFile = null;
+        File path;
         try {
-            screenShotPathToFile = new File(TEMP_PATH + "ScreenShot/" + fileName);
+            path = new File(TEMP_PATH, "ScreenShot");
+        } catch (Exception e) {
+            log.error(e);
+            return;
+        }
+        File screenShotPathToFile;
+        try {
+            log.debug(path.getAbsolutePath());
+            screenShotPathToFile = new File(path + fileName);
         } catch (Exception e) {
             log.error(e);
             return;
@@ -45,33 +54,28 @@ public class ScreenShotMaker extends AbstractVerticle {
 
         try {
             ImageIO.write(screenShot, "jpg", screenShotPathToFile);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             log.error(e);
             return;
         }
 
         openScreenShotFolder();
 
-}
+    }
 
     public void openScreenShotFolder() {
         Desktop desktop;
         if (Desktop.isDesktopSupported()) {
             desktop = Desktop.getDesktop();
-        }else {
+        } else {
             log.warn("Desktop is not supported");
             return;
         }
         try {
-            desktop.open((new File(System.getProperty("java.io.tmpdir") + "ScreenShot/")));
+            desktop.open(new File(TEMP_PATH + "ScreenShot"));
         } catch (IOException e) {
             log.error(e);
             return;
         }
-    }
-    public static void createDir(String path){
-        File f = new File(path);
-        f.mkdirs();
     }
 }
