bd5a8839210aff644127ae33ef9f16ba353862f9
morfeusys <morfeusys@gmail.com>
2016-04-03 18:18:07 +0300

    Browser API upgrade, configurable sites

--- modules/browser/src/main/java/com/aggregate/browser/Browser.java
+++ modules/browser/src/main/java/com/aggregate/browser/Browser.java
@@ -4,33 +4,27 @@ import com.aggregate.api.Markup;
 import com.aggregate.api.Request;
 import io.vertx.core.AbstractVerticle;
 
-import java.awt.Desktop;
-import java.net.URI;
-
 /**
  * Created by morfeusys on 24.02.16.
  */
 public class Browser extends AbstractVerticle {
-    private static final String PATTERN_SITE = "BrowserSite";
+    private static final String BROWSER_SITE_URL = "BrowserSiteURL";
+    private static final String BROWSER_SITE_NAME = "BrowserSiteName";
 
     @Override
     public void start() throws Exception {
-        vertx.eventBus().consumer("cmd.browser.open", m -> onOpen(Request.fromMessage(m)));
+        vertx.eventBus().consumer("cmd.browser.open", m -> m.reply(getSite(Request.fromMessage(m))));
     }
 
-    private void onOpen(Request request) {
-        Markup markup = request.markup.get(PATTERN_SITE);
-        String url = markup != null ? markup.source : request.markup.source;
-        open(url.replace(" ", ""));
-    }
-
-    private void open(String url) {
-        if (Desktop.isDesktopSupported()) {
-            try {
-                Desktop.getDesktop().browse(new URI("http://" + url));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    private String getSite(Request request) {
+        String url;
+        Markup markup = request.markup.get(BROWSER_SITE_URL);
+        if (markup != null) {
+            url = markup.source.replace(" ", "");
+        } else {
+            markup = request.markup.get(BROWSER_SITE_NAME);
+            url = markup.value.replace("_", ".");
         }
+        return url.startsWith("http") ? url : "http://" + url;
     }
 }
