d5b7cdd3b6cc9e81e15a2d16ccb27d5039ff0286
morfeusys <morfeusys@gmail.com>
2016-04-04 20:14:20 +0300

    Google ASR automatic speech detection

--- modules/asr-google/src/main/java/com/aggregate/asr/google/SpeechRecognizer.java
+++ modules/asr-google/src/main/java/com/aggregate/asr/google/SpeechRecognizer.java
@@ -27,7 +27,11 @@ public class SpeechRecognizer extends AbstractVerticle {
     private static final float SAMPLE_RATE = 16000;
     private static final int SAMPLE_SIZE = 16;
     private static final int CHANNELS = 1;
+
     private static final long MIN_LENGTH = 400;
+    private static final long SILENCE_TIMEOUT = 1200;
+    private static final long MIN_SPEECH = 1000;
+
     private static final String LANG = "ru-ru";
 
     private static final String URL = "https://www.google.com/speech-api/v2/recognize?output=json";
@@ -38,9 +42,18 @@ public class SpeechRecognizer extends AbstractVerticle {
     private TargetDataLine myDataLine;
 
     private volatile boolean myListening;
+    private volatile boolean myStop;
     private volatile boolean myError;
     private volatile boolean myCanceled;
 
+    private long minSpeech;
+    private long silenceTimeout;
+
+    private volatile long startTimestamp;
+    private volatile long silenceTimestamp;
+    private volatile int silenceLevel;
+
+    private AudioFormat format;
     private RecorderThread myRecorderThread;
     private RecognizerThread myRecognizerThread;
 
@@ -49,11 +62,13 @@ public class SpeechRecognizer extends AbstractVerticle {
         myApiKey = config().getString("key");
         myLang = config().getString("lang", LANG);
         myMinLength = config().getLong("length", MIN_LENGTH);
+        minSpeech = config().getLong("minSpeech", MIN_SPEECH);
+        silenceTimeout = config().getLong("silenceTimeout", SILENCE_TIMEOUT);
         if (myApiKey == null || myApiKey.isEmpty()) {
             f.fail("Provide your Google ASR API key");
             return;
         }
-        AudioFormat format = new AudioFormat(SAMPLE_RATE, SAMPLE_SIZE, CHANNELS, true, false);
+        format = new AudioFormat(SAMPLE_RATE, SAMPLE_SIZE, CHANNELS, true, false);
         DataLine.Info dataLineInfo = new DataLine.Info(TargetDataLine.class, format);
         Mixer.Info[] infos = AudioSystem.getMixerInfo();
         TargetDataLine dataLine = null;
@@ -70,22 +85,11 @@ public class SpeechRecognizer extends AbstractVerticle {
             return;
         }
         myDataLine = dataLine;
-        try {
-            myDataLine.open(format);
-        } catch (LineUnavailableException e) {
-            f.fail(e);
-            return;
-        }
 
-        vertx.eventBus().consumer("asr.start", e -> {
-           listen();
-        });
-        vertx.eventBus().consumer("asr.stop", e -> {
-           finish();
-        });
-        vertx.eventBus().consumer("asr.cancel", e -> {
-           cancel();
-        });
+        vertx.eventBus().consumer("asr.listen", e -> toggle());
+        vertx.eventBus().consumer("asr.start", e -> listen());
+        vertx.eventBus().consumer("asr.stop", e -> finish());
+        vertx.eventBus().consumer("asr.cancel", e -> cancel());
 
         f.complete();
     }
@@ -97,12 +101,24 @@ public class SpeechRecognizer extends AbstractVerticle {
 
     public void destroy() {
         cancel();
-        myDataLine.close();
+        if (myDataLine.isOpen()) {
+            myDataLine.close();
+        }
+    }
+
+    public synchronized void toggle() {
+        if (myListening && myStop) {
+            finish();
+        } else {
+            listen();
+            myStop = true;
+        }
     }
 
     public synchronized void listen() {
         if (myListening) return;
         myListening = true;
+        myStop = false;
         myError = false;
         myCanceled = false;
         ArrayBlockingQueue<byte[]> queue = new ArrayBlockingQueue<>(8192);
@@ -117,6 +133,8 @@ public class SpeechRecognizer extends AbstractVerticle {
         if (myRecorderThread != null && myRecognizerThread != null) {
             myRecorderThread.finish();
             myRecognizerThread.finish();
+            myRecorderThread = null;
+            myRecognizerThread = null;
         }
     }
 
@@ -129,6 +147,34 @@ public class SpeechRecognizer extends AbstractVerticle {
         myError = true;
         vertx.eventBus().publish("asr.error", error);
     }
+
+    private void onRmsCalculated(int rms) {
+        long now = System.currentTimeMillis();
+        if (startTimestamp == 0) startTimestamp = now;
+        if (rms >= silenceLevel) {
+            silenceLevel = rms;
+            silenceTimestamp = now;
+        } else if (now - startTimestamp > minSpeech) {
+            if (now - silenceTimestamp > silenceTimeout && myRecorderThread != null) {
+                finish();
+            }
+        }
+    }
+
+
+    private static int calculateRMSLevel(byte[] data) {
+        long lSum = 0;
+        for (byte aData1 : data) lSum = lSum + aData1;
+
+        double dAvg = lSum / data.length;
+
+        double sumMeanSquare = 0d;
+        for (byte aData : data) sumMeanSquare = sumMeanSquare + Math.pow(aData - dAvg, 2d);
+
+        double averageMeanSquare = sumMeanSquare / data.length;
+        return (int)(Math.pow(averageMeanSquare, 0.5d) + 0.5);
+    }
+
 
     private class RecorderThread extends Thread {
         private BlockingQueue<byte[]> queue;
@@ -146,13 +192,20 @@ public class SpeechRecognizer extends AbstractVerticle {
         @Override
         public void run() {
             try {
+                myDataLine.open(format);
                 myDataLine.start();
+                silenceLevel = 0;
+                silenceTimestamp = 0;
+                startTimestamp = 0;
                 vertx.eventBus().publish("asr.state", "start");
                 while (!stopped && !myError) {
                     record();
                 }
+            } catch (LineUnavailableException e) {
+                log.error("Cannot access microphone", e);
             } finally {
                 myDataLine.stop();
+                myDataLine.close();
             }
             if (!myCanceled && !myError) {
                 vertx.eventBus().publish("asr.state", "end");
@@ -163,6 +216,9 @@ public class SpeechRecognizer extends AbstractVerticle {
             int len = myDataLine.read(buffer, 0, buffer.length);
             if (len > 0) {
                 queue.add(Arrays.copyOf(buffer, len));
+                if (myStop) {
+                    onRmsCalculated(calculateRMSLevel(buffer));
+                }
             }
         }
     }
